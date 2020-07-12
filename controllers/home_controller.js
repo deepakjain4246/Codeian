e5d530fb0dd6d26c8fb4b9d3d9cb481891f4a6f5
Deepak Jain <Deepak.Jain@rxlogix.com>
2020-07-12 22:25:02 +0530

    Rendering home.ejs

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,13 @@
-module.exports.home=function(req,res){
-  return res.end(`<h1>Express is up for Codeian</h1>`)
-}
+// module.exports.home=function(req,res){
+//   return res.end(`<h1>Express is up for Codeian</h1>`)
+// }
 
+
+
+module.exports.home=function(req,res){
+  return res.render('home',{
+    title:"Home"
+  })
+}
 
 //module.exports.actionName=function(){}
