9e6b5af6e7bc1fa60168a0c8d52e7e8009b30d0a
Deepak Jain <Deepak.Jain@rxlogix.com>
2020-07-12 17:31:48 +0530

    Fired express server and added different directories

---
+++ index.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const app=express();
+const port=8000;
+
+
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error in running on port:${port}`);
+    }
+    console.log(`Server is up and running on port ${port}`);
+});
