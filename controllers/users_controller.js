5f70169cd219ba7a90d7dc8fc0abcff77e5cda37
Deepak Jain <Deepak.Jain@rxlogix.com>
2020-07-12 22:52:08 +0530

    added partials i.e headers and footers

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,6 +1,12 @@
-module.exports.profile=function(req,res){
-    return res.end(`<h1>Users profile</h1>`)
-  }
+// module.exports.profile=function(req,res){
+//     return res.end(`<h1>Users profile</h1>`)
+//   }
   
+
+  module.exports.profile=function(req,res){
+    return res.render('users_profile',{
+      title:"Profile"
+    })
+  }
   
   //module.exports.actionName=function(){}
