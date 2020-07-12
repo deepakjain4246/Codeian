06f02268299c8fa621c5913dd80155787987d642
Deepak Jain <Deepak.Jain@rxlogix.com>
2020-07-12 18:40:07 +0530

    Added one more route and controoller

---
+++ controllers/users_controller.js
@@ -0,0 +1,6 @@
+module.exports.profile=function(req,res){
+    return res.end(`<h1>Users profile</h1>`)
+  }
+  
+  
+  //module.exports.actionName=function(){}
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express=require('express');
 const app=express();
 const port=8000;
 
+//use the express server
+app.use('/',require('./routes/index.js'))
+
 
 app.listen(port,function(err){
     if(err){
