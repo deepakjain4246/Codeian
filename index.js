d3d39a253f60c22790b707bd82216c11475b1840
Deepak Jain <Deepak.Jain@rxlogix.com>
2020-07-12 22:07:51 +0530

    Setting up the view engine ejjs

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port=8000;
 //use the express server
 app.use('/',require('./routes/index.js'))
 
+//set up the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
 
 app.listen(port,function(err){
     if(err){
