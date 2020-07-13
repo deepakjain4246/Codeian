795a194c5a5059ec874b9620eabcbc58ebb6c983
Deepak Jain <Deepak.Jain@rxlogix.com>
2020-07-13 23:28:53 +0530

    added sign-up and sign-in faction

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -9,4 +9,18 @@
     })
   }
   
+  //render the sign up page
+  module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+      title:"SignUp"
+    })
+  }
+
+  //render the sign in page
+  module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+      title:"SignIn"
+    })
+  }
+
   //module.exports.actionName=function(){}
