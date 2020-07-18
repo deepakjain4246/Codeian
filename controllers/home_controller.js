63dafcbd8786f77538c5cd82eceb1870cb22f995
Deepak Jain <Deepak.Jain@rxlogix.com>
2020-07-18 18:29:42 +0530

    set the cookie

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -5,6 +5,8 @@
 
 
 module.exports.home=function(req,res){
+  console.log(req.cookies);  //manually set the cookie in inspect appli and check throuh here
+  res.cookie('user_id',10); //way to send the cookie to server side ...can be checked through inspect application
   return res.render('home',{
     title:"Home"
   })
