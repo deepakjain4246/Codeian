998831668b409d888324db4d327554f85dd1bfdc
Deepak Jain <Deepak.Jain@rxlogix.com>
2020-07-18 19:18:31 +0530

    created sign-up page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,7 +2,7 @@
 //     return res.end(`<h1>Users profile</h1>`)
 //   }
   
-
+const User=require('../models/user')
   module.exports.profile=function(req,res){
     return res.render('users_profile',{
       title:"Profile"
@@ -24,3 +24,34 @@
   }
 
   //module.exports.actionName=function(){}
+
+
+  //get the sign-up data
+  module.exports.create=function(req,res){
+    if(req.body.password !=req.body.confirm_password){
+      return res.redirect('back');
+    }
+    User.findOne({email:req.body.email},function(err,user){
+                  if(err){
+                    console.log('error in finding user in signing-up') 
+                    return;
+                  }
+           if(!user){
+             User.create(req.body,function(err,user){
+               if(err){
+              console.log('error in creating user while signing-up')
+              return;
+             }
+             res.redirect('/users/sign-in');
+            })
+           }
+           else{
+             res.redirect('back');
+           }
+    });
+  }
+
+  //sign in and create the session 
+  module.exports.createSession=function(req,res){
+    //TODO later
+  }
