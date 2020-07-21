614e674e1c77e3394f258fc3015afaefb23139b5
Deepak Jain <Deepak.Jain@rxlogix.com>
2020-07-21 18:07:06 +0530

    added delete button for post.

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post=require('../models/posts');
+const Comment = require('../models/comment');
 
 module.exports.create=function(req,res){
     Post.create({
@@ -8,4 +9,20 @@ module.exports.create=function(req,res){
         if(err){console.log('error in creating a post');return;}
     return res.redirect('back');
 });
+}
+
+module.exports.destroy=function(req,res){
+    Post.findById(req.params.id,function(err,post){
+
+        //.id means converting object id into string id
+        if(post.user == req.user.id){
+            post.remove();
+            Comment.deleteMany({post: req.params.id},function(err){
+                return res.redirect('back');
+            });
+        }
+        else{
+            return res.redirect('back');
+        }
+    });
 }
