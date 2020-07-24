807e363d4b7642f02a705742d74f4c5c7dc46084
Deepak Jain <Deepak.Jain@rxlogix.com>
2020-07-24 22:23:12 +0530

    optimizing by adding async and await.

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -7,38 +7,64 @@ const User=require('../models/user')
 
 
 
-module.exports.home=function(req,res){
-  // console.log(req.cookies);  //manually set the cookie in inspect appli and check throuh here
-  // res.cookie('user_id',10); //way to send the cookie to server side ...can be checked through inspect application
-  // return res.render('home',{
-  //   title:"Home"
-  // })
+// module.exports.home=function(req,res){
+//   // console.log(req.cookies);  //manually set the cookie in inspect appli and check throuh here
+//   // res.cookie('user_id',10); //way to send the cookie to server side ...can be checked through inspect application
+//   // return res.render('home',{
+//   //   title:"Home"
+//   // })
 
-  // Post.find({},function(err,posts){
-  //   return res.render('home',{
-  //     title:'Codial | Home',
-  //     posts: posts
-  //   });
-  // });
+//   // Post.find({},function(err,posts){
+//   //   return res.render('home',{
+//   //     title:'Codial | Home',
+//   //     posts: posts
+//   //   });
+//   // });
   
-  Post.find({})
-  .populate('user')
-  .populate({
-    path:'comments',
-    populate:{
-      path:'user'
-    }
-  })
-  .exec(function(err,posts){
+//   Post.find({})
+//   .populate('user')
+//   .populate({
+//     path:'comments',
+//     populate:{
+//       path:'user'
+//     }
+//   })
+//   .exec(function(err,posts){
     
-    User.find({},function(err,users){
-      return res.render('home',{
-        title:'Codial | Home',
-        posts: posts,
-        all_users:users
-    });
-  });
+//     User.find({},function(err,users){
+//       return res.render('home',{
+//         title:'Codial | Home',
+//         posts: posts,
+//         all_users:users
+//     });
+//   });
+// });
+// }
+
+
+module.exports.home=async function(req,res){
+
+  try{
+    let posts= await Post.find({})
+.populate('user')
+.populate({
+  path:'comments',
+  populate:{
+    path:'user'
+  }
 });
+
+let users= await User.find({});
+
+return res.render('home',{
+  title:'Codial | Home',
+  posts: posts,
+  all_users:users
+});
+  }catch(err){
+    console.log('Error:'+err);
+    return;
+  }
 }
 
 //module.exports.actionName=function(){}
